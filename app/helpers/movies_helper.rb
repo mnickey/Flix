ed4d546e8972c7021c94134374cc27e6f86692fd
Michael Nickey <mnickey@gmail.com>
2014-02-11 22:55:29 -0800

    Ready to deploy version 1.0

---
+++ app/helpers/movies_helper.rb
@@ -0,0 +1,35 @@
+module MoviesHelper
+ def format_total_gross(movie)
+   if movie.flop?
+    content_tag(:strong, 'Flop!')
+   else
+     number_to_currency(movie.total_gross)
+   end
+ end
+ 
+ def image_for(movie)
+   if movie.image_file_name.blank?
+     image_tag('placeholder.png')
+   else
+     image_tag(movie.image_file_name)
+   end
+ end
+ 
+ def delete_review_image
+   image_tag('trash.png', style: 'border: 0', :title => 'Delete Review')
+ end
+ 
+ def format_average_stars(movie)   
+   if movie.average_stars.nil?
+     content_tag(:strong, 'No reviews')
+   else
+     # "*" * movie.average_stars.round
+     pluralize(number_with_precision(movie.average_stars, :precision => 1) , 'star')
+   end
+ end
+
+  def recent_reviews
+    reviews.order('created_at desc').limit(2)
+  end
+ 
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Flix::Application.routes.draw do
+  
+  root "movies#index"
+  resources :movies do
+  	resources :reviews
+  end
+end
